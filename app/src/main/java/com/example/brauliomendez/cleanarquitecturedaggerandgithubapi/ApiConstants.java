527a57ca037c1a14e86ed0e5351cd10cf9b95029
brauliomendez <braauliomendez@ciencias.unam.mx>
2017-01-24 14:19:51 -0600

    NEW: Added Item And searchResponse Models.

--- app/src/main/java/com/example/brauliomendez/cleanarquitecturedaggerandgithubapi/ApiConstants.java
+++ app/src/main/java/com/example/brauliomendez/cleanarquitecturedaggerandgithubapi/ApiConstants.java
@@ -9,4 +9,6 @@ public class ApiConstants {
     public static final String BASE_URL = "https://api.github.com/";
 
     public static final String USER_ENDPOINT = "users/{user_id}";
+
+    public static final String SEARCH_REPOSITORIES_ENDPOINT = "search/repositories?q={keyword}";
 }
